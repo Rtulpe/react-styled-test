981404b284a0b1e229312a8cdd314e81c9ce2c82
Trust <r.tolpeznikas@gmail.com>
2024-01-01 02:36:47 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import { StyledDiv } from './StyledComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, React + Styled Components!</h1>
+      <StyledDiv>This is a styled component.</StyledDiv>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/StyledComponent.js
@@ -0,0 +1,8 @@
+import styled from "styled-components"
+
+export const StyledDiv = styled.div`
+    color: #333;
+    padding: 20px;
+    background-color: #eee;
+    border-radius: 5px;
+`;
